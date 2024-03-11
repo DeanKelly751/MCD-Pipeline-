7c253e8241286b21264c12b9a5a0cd6c3cce1c19
Ray Carroll <rcarroll@redhat.com>
2024-03-11 09:20:13 +0000

    Update deploy scripts

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
-
-## This script is executed after the CODECO operator deployment finishes. It is used to install additional 
+## This script is executed after the CODECO operator deployment finishes. It is used to install additional
 ## components and to perform any other post-deployment tasks.
-
 echo "Executing post deployment tasks..."
-
 ##TODO(user): Add your post deployment tasks here
+cd ..
+echo ".....................Installing SWM....................................."
+cd qos-scheduler
+make chart
+helm install qostest --namespace=codeco-swm-controllers --create-namespace tmp/helm
+cd ..
+echo "......................................Finished installing SWM.................................."
+echo ".....................Installing MDM....................................."
+cd mdm-api
+export MDM_NAMESPACE=mdm
+export MDM_CONTEXT=kind-kind
+sudo kubectl --context=$MDM_CONTEXT create namespace $MDM_NAMESPACE
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add neo4j https://helm.neo4j.com/neo4j
+sed -i "s/<storageclassName>/standard/g" "./deployment/zookeeper-helm.yaml"
+sed -i "s/<storageclassName>/standard/g" "./deployment/neo4j-helm.yaml"
+helm --kube-context=$MDM_CONTEXT install mdm-zookeeper -n $MDM_NAMESPACE  bitnami/zookeeper  -f ./deployment/zookeeper-helm.yaml
+helm --kube-context=$MDM_CONTEXT install mdm-kafka -n $MDM_NAMESPACE bitnami/kafka --version 21.1.1 -f ./deployment/kafka-helm.yaml
+helm --kube-context=$MDM_CONTEXT install mdm-neo4j -n $MDM_NAMESPACE neo4j/neo4j-standalone -f ./deployment/neo4j-helm.yaml
+sudo kubectl --context=$MDM_CONTEXT -n $MDM_NAMESPACE exec -i mdm-kafka-0 -- /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server mdm-kafka-0:9093 --create --topic json-events --config cleanup.policy=compact
+helm --kube-context=$MDM_CONTEXT -n $MDM_NAMESPACE install mdm-controller ./controller/src/helm -f ./controller/src/helm/values.yaml
+helm --kube-context=$MDM_CONTEXT -n $MDM_NAMESPACE install mdm-api ./mdm-api/src/helm -f ./mdm-api/src/helm/values.yaml
+cd ..
+echo "........................................Finished installing MDM..............................................."
+# export POD_NAME=$(sudo kubectl get pods --namespace mdm -l "app.kubernetes.io/name=mdm-api,app.kubernetes.io/instance=mdm-api" -o jsonpath="{.items[0].metadata.name}")
+# export CONTAINER_PORT=$(sudo kubectl get pod --namespace mdm $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+# sudo kubectl get all -n codecoapp-operator-system
+# sudo kubectl get nodes
+# sudo kubectl get networklinks -A
+# sudo kubectl get networkpaths -A
+# sudo kubectl --namespace mdm port-forward $POD_NAME 9092:$CONTAINER_PORT
+echo ".....................Installing PDLC....................................."
+cd pdlc-pp
+# chmod -R 777 .
+# sudo ./apply-controller.sh
+# cd crd\ data\ extraction/
+# sudo pip3 install -r requirements_e.txt
+# sudo python3 ca_crd_extractor.py
+cd ..
+echo "........................................Finished installing PDLC..............................................."
--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
 ## This script is executed before the CODECO operator deployment starts. It is used to install dependencies or
 ## other components' dependencies
-
 echo "Installing pre deployment dependencies..."
-
+git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/scheduling-and-workload-migration-swm/qos-scheduler.git
+git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/metadata-manager-mdm/mdm-api.git
+git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/privacy-preserving-decentralised-learning-and-context-awareness-pdlc/context-awareness/pdlc-pp.git
+git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/network-management-and-adaptation-netma/secure-connectivity.git
+git clone https://github.com/k8snetworkplumbingwg/multus-cni.git
 ##TODO(user): Add your dependencies here
