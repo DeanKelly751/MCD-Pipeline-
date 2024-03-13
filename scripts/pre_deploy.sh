bf03630a419fdf07c7087a60f52fb3fb868e3336
Dean Kelly <dekelly@redhat.com>
2024-03-13 16:10:17 +0000

    Update pre_deploy.sh

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+
 ## This script is executed before the CODECO operator deployment starts. It is used to install dependencies or
 ## other components' dependencies
-cd ..
 
+# Function to clone repository if not already present
+clone_if_not_exists() {
+    local repo_url=$1
+    local target_dir=$2
+
+    if [ ! -d "$target_dir" ]; then
+        git clone "$repo_url" "$target_dir"
+    else
+        echo "Directory $target_dir already exists. Skipping cloning."
+    fi
+}
+
+# Create Kubernetes cluster
 sudo kind create cluster --config ./config/cluster/kind-config.yaml
 
-git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/scheduling-and-workload-migration-swm/qos-scheduler.git
-git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/metadata-manager-mdm/mdm-api.git
-git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/privacy-preserving-decentralised-learning-and-context-awareness-pdlc/context-awareness/pdlc-pp.git
-git clone https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/network-management-and-adaptation-netma/secure-connectivity.git
-git clone https://github.com/k8snetworkplumbingwg/multus-cni.git
+cd ..
+
+# Clone repositories if not already present
+clone_if_not_exists "https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/scheduling-and-workload-migration-swm/qos-scheduler.git" "qos-scheduler"
+clone_if_not_exists "https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/metadata-manager-mdm/mdm-api.git" "mdm-api"
+clone_if_not_exists "https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/privacy-preserving-decentralised-learning-and-context-awareness-pdlc/context-awareness/pdlc-pp.git" "pdlc-pp"
+clone_if_not_exists "https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/network-management-and-adaptation-netma/secure-connectivity.git" "secure-connectivity"
+clone_if_not_exists "https://github.com/k8snetworkplumbingwg/multus-cni.git" "multus-cni"
+
 ##TODO(user): Add your dependencies here
