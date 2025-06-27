90b9d2dc47f0dafaf9c5b0bbd008c252380bfbf6
Konstantinos Karageorgos <karagk@intracom-telecom.com>
2025-06-27 09:48:30 +0000

    Edit post_deploy.sh

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -177,7 +177,7 @@ yq eval '.volumePermissions.image = {
 
 # network policy fix - mdm access to prometheus - for k3s installations
 kubectl label namespace he-codeco-mdm namespace=he-codeco-mdm
-yq '.spec.ingress += load("acm/scripts/network-rule-mdm.yaml")' \
+yq '.spec.ingress += load("../acm/scripts/network-rule-mdm.yaml")' \
   new-prometheus-networkPolicy.yaml > new-prometheus-networkPolicy.yaml
 kubectl apply -f new-prometheus-networkPolicy.yaml
 #
